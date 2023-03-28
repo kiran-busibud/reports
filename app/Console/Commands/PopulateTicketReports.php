3de7744833e83eed4a76ccf58453277ee2de2a12
kiran-busibud <kiran@zinosi.com>
2023-03-28 16:54:08 +0530

    HELP-10183 #time 2h scheduler for unresolved tickets with tags.

--- app/Console/Commands/PopulateTicketReports.php
+++ app/Console/Commands/PopulateTicketReports.php
@@ -27,12 +27,45 @@ class PopulateTicketReports extends Command
      */
     public function handle(): void
     {
-        $tickets = [];
+
+        $params = [];
         $unresolved_ticket_status_ids = [4,6];
-        $$tickets = DB::table('hl_ticket')
-                    ->select('id','ticket_status_id','ticket_channel','ticket_agent','ticket_brand_id','ticket_title','ticket_description')
-                    ->whereIn('ticket_status_id', $unresolved_ticket_status_ids)
-                    ->get();
-        dd($tickets);
+        $params = array_merge($params, $unresolved_ticket_status_ids);
+        $query = "SELECT id,ticket_status_id,ticket_channel,ticket_agent,ticket_brand_id,ticket_title,ticket_description
+                    FROM hl_ticket 
+                    WHERE ticket_status_id in (" . implode(',', array_fill(0, count($unresolved_ticket_status_ids), '?')) . ")";
+        
+        // $tickets = [];
+        // $unresolved_ticket_status_ids = [4,6];
+        // $$tickets = DB::table('hl_ticket')
+        //             ->select('id','ticket_status_id','ticket_channel','ticket_agent','ticket_brand_id','ticket_title','ticket_description')
+        //             ->whereIn('ticket_status_id', $unresolved_ticket_status_ids)
+        //             ->get();
+        $tickets = DB::select($query,$params);
+        // dd($tickets);
+
+        $params = [];
+        $query = "SELECT ticket_id, meta_value
+                    FROM hl_ticketmeta
+                    WHERE meta_key = 'tags'";
+        $tags = DB::select($query,$params);
+        $tag_mapping = [];
+        foreach($tags as $tag){
+            $tag_mapping[$tag->ticket_id] = $tag->meta_value;
+        }
+        // dd(count($tags));
+
+        foreach($tickets as $ticket){
+            DB::table('ticket_reports')->insert([
+                'ticket_title' => $ticket->ticket_title,
+                'ticket_description' => $ticket->ticket_description,
+                'ticket_agent' => $ticket->ticket_agent,
+                'ticket_status_id' => $ticket->ticket_status_id,
+                'ticket_brand_id' => $ticket->ticket_brand_id,
+                'ticket_channel' => $ticket->ticket_channel,
+                'ticket_tags' => $tag_mapping[$ticket->id] ?? ""
+            ]);
+        }
+        // dd(gettype($tickets[0]->id));
     }
 }
