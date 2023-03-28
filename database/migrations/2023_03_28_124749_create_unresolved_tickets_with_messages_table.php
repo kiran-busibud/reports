185a67e1580979be723f5f0ca43405540583a85c
kiran-busibud <kiran@zinosi.com>
2023-03-28 19:59:30 +0530

    HELP-10190 #Time 2h precaching  unresolved tickets with messages

---
+++ database/migrations/2023_03_28_124749_create_unresolved_tickets_with_messages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('unresolved_tickets_with_messages', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('ticket_title');
+            $table->longText('ticket_description');
+            $table->bigInteger('ticket_agent')->length(20);
+            $table->bigInteger('ticket_status_id')->length(20);
+            $table->bigInteger('ticket_brand_id')->length(20);
+            $table->bigInteger('ticket_channel')->length(20);
+            $table->longText('ticket_tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('unresolved_tickets_with_messages');
+    }
+};
